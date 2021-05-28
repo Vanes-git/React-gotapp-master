cff964270dd4befa65ba8fefae44b76055d92c60
Ioann <vanes.sysoev@gmail.com>
2021-05-28 15:57:01 +0300

    feat: randChar(API)
    working with API

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+    return (
+        <div class='loadingio-spinner-spin-kad0x17rphp'>
+            <div class='ldio-l22fhjfsbc'>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+                <div>
+                    <div />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
